8e7832e199c6c7ac7a11428f38778719d1a8924b
whywhs <46738358+whywhs@users.noreply.github.com>
2019-01-16 17:52:47 +0800

    Create Densenet.py

---
+++ Densenet.py
@@ -0,0 +1,84 @@
+'''
+Python 3.6 
+Pytorch 0.3
+This project is produced by Hongyu Wang in June 2018 at MSRA.
+And really thanks to Dr. Jianshu Zhang from USTC for useful comments and suggestions.
+'''
+import torch
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_planes, growth_rate):
+        super(Bottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, 4*growth_rate, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(4*growth_rate)
+        self.conv2 = nn.Conv2d(4*growth_rate, growth_rate, kernel_size=3,padding=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(x))
+        out = self.conv2(F.relu(out))
+        out = torch.cat([out,x], 1)
+        return out
+
+class Transition(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super(Transition, self).__init__()
+        self.bn = nn.BatchNorm2d(in_planes)
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv(F.relu(x))
+        out = F.avg_pool2d(out, 2)
+        return out
+
+class DenseNet(nn.Module):
+    def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_classes=10):
+        super(DenseNet, self).__init__()
+        self.growth_rate = growth_rate
+
+        num_planes = 2*growth_rate
+        self.conv1 = nn.Conv2d(1, num_planes, kernel_size=7, stride=2, bias=False)
+        self.max_pool = nn.MaxPool2d(2)
+
+        self.dense1 = self._make_dense_layers(block, num_planes, nblocks[0])
+        num_planes += nblocks[0]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans1 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense2 = self._make_dense_layers(block, num_planes, nblocks[1])
+        num_planes += nblocks[1]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans2 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense3 = self._make_dense_layers(block, num_planes, nblocks[2])
+        num_planes += nblocks[2]*growth_rate
+
+        # self.my_conv = nn.Conv2d(342,128,kernel_size=3,stride=1,padding=1 )
+
+    def _make_dense_layers(self, block, in_planes, nblock):
+        layers = []
+        for i in range(nblock):
+            layers.append(block(in_planes, self.growth_rate))
+            in_planes += self.growth_rate
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.max_pool(out)
+        out = self.trans1(self.dense1(out))
+        out = self.trans2(self.dense2(out))
+        out = self.dense3(out)
+        # out = self.dense4(out)
+        # out = self.my_conv(out)
+        #out = F.avg_pool2d(F.relu(out),4)
+        #out = F.relu(out)           #684*12*bb
+        return out
+
+def DenseNet121():
+    # return DenseNet(Bottleneck, [6,12,24,16], growth_rate=12)
+    return DenseNet(Bottleneck, [16,16,16], growth_rate=24)
